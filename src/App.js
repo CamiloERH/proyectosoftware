7003f5a17c1b93756671ecdb0ac2015badc899b2
italin131 <italin131@gmail.com>
2021-09-21 14:04:42 -0300

    probando nuevamente

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route exact path="/" component={() => 
             <div>
               <h1>Homeeee</h1>
-              <Login/>
+              <p>Probando esto </p>
 
             </div>}
           />
