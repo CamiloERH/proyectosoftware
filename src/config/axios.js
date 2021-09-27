4a293c8e5ddc1c5a32a84e1220d80d6892afd535
Camilo <rojash.camiloe@gmail.com>
2021-09-27 08:48:39 -0300

    variable de entorno

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
     // baseURL: 'https://agendaappserver.herokuapp.com/'
-    baseURL: 'http://localhost:4000/'
+    baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
 export default clienteAxios;
