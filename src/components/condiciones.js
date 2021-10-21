e287912866502588bd9f9a25d7e5c12e19c5f9e4
johan.fuentes <johan.fuentes@mail.udp.cl>
2021-10-20 22:46:48 -0300

    update condiciones

--- src/components/condiciones.js
+++ src/components/condiciones.js
@@ -1,12 +1,42 @@
+import "./condiciones.css";
+
 function Condiciones(){
 
     return (
-        <div>
+        <div className="container">
 
-            <h1>Probando condiciones</h1>
+            <h1 className="text-center titulo">Condiciones de los servicios</h1>
+
+
+            <ol className="lista">
+
+                <li> Se agenda con un previo abono del 50% el cual no es reembolsable solo en caso de informar 48 horas antes que no podrá asistir.</li>
+
+                <li> Se realizan maximo 2 retoques siempre y cuando las pestañas se encuentren en óptimas condiciones y tengan un mínimo del 40% de las extensiones en cada ojo y dentro de 15 a 20 días desde la postura anterior.</li>
+
+                <li> Solo se aceptarán 5 minutos de retraso estando en la puerta (después de los 5 minutos la hora quedará inmediatamente cancelada sin derecho a reclamos).</li>
+
+                <li> No pueden venir con maquillaje.</li>
+
+                <li> No se permiten acompañantes.</li>
+
+                <li> La dirección se entregará después de realizado el abono por temas de seguridad.</li>
+
+                <li> En caso de transferir a una cuenta errónea será su responsabilidad.</li>
+
+                <li> Como parte del procedimiento puede llegar a existir irritación ocular, dolor ocular, comezón en los ojos, malestar, y en casos excepcionales infección ocular.</li>
+
+                <li> Aunque el técnico pueda aplicar y quitar las pestañas correctamente,los materiales adhesivos pueden alojarse durante o después del procedimiento, lo que puede irritar mis ojos o requerir más cuidados de seguimiento.</li>
+
+                <li> Para el caso de la extensiones de pestañas, se tendra que estar 180 minutos aproximadamente con los ojos cerrados durante el procedimiento. También debe estar acostada en posición reclinada.</li>
+
+                <li> La persona que se realize el tratamiento debe ser mayor de edad, en caso contrario debe haber un padre o tutor que acepte todas las condiciones antes escritas.</li>
+
+            </ol>
 
         </div>
     )
 }
 
 export default Condiciones
+
